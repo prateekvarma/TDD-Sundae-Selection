2ce7baddf2e24035b1141d110b4e306ed24a570a
Prateek Varma <prtk.varma@gmail.com>
2022-08-08 13:00:47 -0600

    Changed eslint indent config to 2 spaces and applied fix

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -2,22 +2,22 @@ import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const SummaryForm = () => {
-	const [tcChecked, setTcChecked] = useState(false);
-	return (
-		<Form>
-			<Form.Group controlId="terms-and-conditons">
-				<Form.Check
-					type="checkbox"
-					checked={tcChecked}
-					onChange={() => setTcChecked(!tcChecked)}
-					label="terms and conditions"
-				/>
-			</Form.Group>
-			<Button variant="primary" type="submit" disabled={!tcChecked}>
+  const [tcChecked, setTcChecked] = useState(false);
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditons">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={() => setTcChecked(!tcChecked)}
+          label="terms and conditions"
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
         Confirm Order
-			</Button>
-		</Form>
-	);
+      </Button>
+    </Form>
+  );
 };
 
 export default SummaryForm;

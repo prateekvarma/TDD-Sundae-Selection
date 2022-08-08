ec0b0d18b4770fa72a4fc0a6b9c4330f41a8b1f0
Prateek Varma <prtk.varma@gmail.com>
2022-08-08 13:40:30 -0600

    Installed testing-lib's userEvent, and replaced it with fireEvent

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,30 +1,31 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 test("initial conditions", () => {
-	render(<SummaryForm />);
+  render(<SummaryForm />);
 
-	//check if there is a checkbox labelled "terms and conditions"
-	const checkbox = screen.getByRole("checkbox", {
-		name: /terms and conditions/i,
-	});
-	expect(checkbox).not.toBeChecked();
+  //check if there is a checkbox labelled "terms and conditions"
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
 
-	//check if there is a button called "confirm order"
-	const button = screen.getByRole("button", { name: /confirm order/i });
-	expect(button).toBeDisabled();
+  //check if there is a button called "confirm order"
+  const button = screen.getByRole("button", { name: /confirm order/i });
+  expect(button).toBeDisabled();
 });
 
 test("Checkbox enables button on first click, and disables on the 2nd", () => {
-	render(<SummaryForm />);
-	const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i });
-	const button = screen.getByRole("button", { name: /confirm order/i });
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i });
+  const button = screen.getByRole("button", { name: /confirm order/i });
 
-	//check the checkbox and see if the button is enabled
-	fireEvent.click(checkbox);
-	expect(button).toBeEnabled();
+  //check the checkbox and see if the button is enabled
+  userEvent.click(checkbox);
+  expect(button).toBeEnabled();
 
-	//click the checkbox again, and expect the button to be disabled
-	fireEvent.click(checkbox);
-	expect(button).toBeDisabled();
+  //click the checkbox again, and expect the button to be disabled
+  userEvent.click(checkbox);
+  expect(button).toBeDisabled();
 });

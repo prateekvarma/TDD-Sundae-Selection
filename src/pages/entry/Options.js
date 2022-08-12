78c1b9ebffc9a23fb9ea877566577a47f8960897
Prateek Varma <prtk.varma@gmail.com>
2022-08-12 13:47:51 -0600

    Deliberately failed mock server requests, and wrote passing tests to find the alert banner in case of failed requests

--- src/pages/entry/Options.js
+++ src/pages/entry/Options.js
@@ -3,19 +3,25 @@ import axios from "axios";
 
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 import { Row } from "react-bootstrap";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   //optionType is either 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if(error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
---
+++ src/pages/entry/tests/OrderEntry.test.js
@@ -0,0 +1,27 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+
+import OrderEntry from "../OrderEntry";
+import { server } from "../../../mocks/server";
+
+test("Handles errros for scoops and toppings routes", async () => {
+  //reset the endpoint, as in "src/setupTests.js" then throw a deliberate error so we can test
+  //read : https://mswjs.io/docs/api/setup-server/reset-handlers
+  server.resetHandlers(
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
+      res(ctx.status(500))
+    ),
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
+      res(ctx.status(500))
+    )
+  );
+
+  render(<OrderEntry />);
+  //find the alerts, since the server is throwing a 500
+  //using "waitFor" because there are 2 async calls instead of 1
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert"); //name param is somehow not getting read
+    expect(alerts).toHaveLength(2);
+  });
+  //above, we're using "find" because we know it's going to be async, because we will catch the error from the ".catch" block from axios
+});

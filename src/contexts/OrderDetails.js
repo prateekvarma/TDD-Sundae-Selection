3b2168caa85476586e42b4684c67aa45f76beccd
Prateek Varma <prtk.varma@gmail.com>
2022-08-15 11:50:54 -0600

    Added price formatting, and now the tests on totalEntry.test.js passes

--- src/contexts/OrderDetails.js
+++ src/contexts/OrderDetails.js
@@ -1,6 +1,15 @@
 import { createContext, useContext, useState, useEffect, useMemo } from "react";
 import { pricePerItem } from "../constants";
 
+//format number as currency
+function formatCurrency(amount) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
+
 const OrderDetails = createContext();
 
 //create custom hook to check whether we're inside a provider
@@ -29,10 +38,12 @@ export function OrderDetailsProvider(props) {
     toppings: new Map(),
   });
 
+  const zeroCurrency = formatCurrency(0);
+
   const [totals, setTotals] = useState({
-    scoops: 0,
-    toppings: 0,
-    grandTotal: 0,
+    scoops: zeroCurrency,
+    toppings: zeroCurrency,
+    grandTotal: zeroCurrency,
   });
 
   useEffect(() => {
@@ -41,9 +52,9 @@ export function OrderDetailsProvider(props) {
     const grandTotal = scoopsSubtotal + toppingsSubtotal;
 
     setTotals({
-      scoops: scoopsSubtotal,
-      toppings: toppingsSubtotal,
-      grandTotal,
+      scoops: formatCurrency(scoopsSubtotal),
+      toppings: formatCurrency(toppingsSubtotal),
+      grandTotal: formatCurrency(grandTotal),
     });
   }, [optionCounts]);
 

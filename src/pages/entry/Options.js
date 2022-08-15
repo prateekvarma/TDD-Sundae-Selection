2ba043450ee62c5b1751132bb63b10dc2e5f2d4b
Prateek Varma <prtk.varma@gmail.com>
2022-08-14 19:48:25 -0600

    Added React code to make the interface - WIP - tests not passing yet

--- src/pages/entry/Options.js
+++ src/pages/entry/Options.js
@@ -4,12 +4,15 @@ import axios from "axios";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 import { Row } from "react-bootstrap";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   //optionType is either 'scoops' or 'toppings'
   useEffect(() => {
@@ -19,19 +22,30 @@ export default function Options({ optionType }) {
       .catch((error) => setError(true));
   }, [optionType]);
 
-  if(error) {
+  if (error) {
     return <AlertBanner />;
   }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase(); //Capitalizes the first letter of the word.
 
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      } 
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>{title} total: {orderDetails.totals[optionType]}</p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
